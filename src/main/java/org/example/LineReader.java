4ea6982163413f170aaf6accf51b27d61889c6b0
Vladimir Mickhaylovich <yalt.vladimir@gmail.com>
2022-11-21 09:49:51 +0300

    Add exception to avoid third action of an expression

--- src/main/java/org/example/LineReader.java
+++ src/main/java/org/example/LineReader.java
@@ -6,47 +6,48 @@ import java.io.InputStreamReader;
 
 public class LineReader {
 
-    private String numFirst;
+    private String numFirst ;
     private String numSecond;
     private String mathSymbol;
 
-    public void Reader(){
+    public void Reader() {
         try {
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             String inputText = bufferedReader.readLine();
-            if(inputText.length()>4 && inputText.length()<10){
-                char [] inputTextArray = inputText.toCharArray();
+            if (inputText.length() > 4 && inputText.length() < 10 && spaceCheck(inputText)) {
+                char[] inputTextArray = inputText.toCharArray();
                 MakeVariables(inputTextArray);
 
-            }else{
-                System.out.println("ввод строки не сответсвует требованиям а + в");
+            } else {
+                throw new IllegalArgumentException("ввод строки не сответсвует требованиям а + в");
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Line is not read");
             e.getStackTrace();
         }
     }
-    private  void MakeVariables(char[]c){
+
+    private void MakeVariables(char[] c) {
         StringBuilder numOne = new StringBuilder();
         StringBuilder symbol = new StringBuilder();
         StringBuilder numTwo = new StringBuilder();
 
-        for(int i = 0;i<c.length;){
-            if(c[i]!=' ' && symbol.isEmpty()){
+        for (int i = 0; i < c.length; ) {
+            if (c[i] != ' ' && symbol.isEmpty()) {
                 numOne.append(c[i]);
                 i++;
-            }else {
-                if(c[i]==' '){
+            } else {
+                if (c[i] == ' ') {
                     i++;
                     symbol.append(c[i]);
                     i++;
                 }
-                if(c[i]==' '){
+                if (c[i] == ' ') {
                     i++;
                     numTwo.append(c[i]);
                     i++;
-                }else {
+                } else {
                     numTwo.append(c[i]);
                     i++;
                 }
@@ -56,20 +57,28 @@ public class LineReader {
         this.numFirst = numOne.toString();
         this.numSecond = numTwo.toString();
         this.mathSymbol = symbol.toString();
-
-      //  System.out.println("test is done");
-
     }
 
-    public String getNumFirst() {
+    private boolean spaceCheck (String line){
+        char[] arrayLine = line.toCharArray();
+        int spaceCounter = 0;
+        for (Character s : arrayLine) {
+            if (s == ' ')
+                spaceCounter++;
+        }
+        if (spaceCounter > 2)
+            return false;
+        return true;
+    }
+    public String getNumFirst () {
         return numFirst;
     }
 
-    public String getNumSecond() {
+    public String getNumSecond () {
         return numSecond;
     }
 
-    public String getMathSymbol() {
+    public String getMathSymbol () {
         return mathSymbol;
     }
 
